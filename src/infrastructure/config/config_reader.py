d674869f12116b08c7c2018c2a384031ea452aa1
Laurenz <ddanilenko350@gmail.com>
2025-12-07 10:36:47 +0200

    Add config_reader

---
+++ src/infrastructure/config/config_reader.py
@@ -0,0 +1,17 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from pathlib import Path
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: SecretStr
+    
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent / ".env",
+        env_file_encoding="UTF-8",
+        extra="forbid"
+    )
+    
+    
+settings = Settings()
