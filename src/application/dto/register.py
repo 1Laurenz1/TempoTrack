bbc40357e212f68bb7ce79f83b1b2cb796b0426a
Laurenz <ddanilenko350@gmail.com>
2025-12-18 19:17:33 +0200

    Add RegisterUserResponse DTO

--- src/application/dto/register.py
+++ src/application/dto/register.py
@@ -3,11 +3,20 @@ from dataclasses import dataclass, field
 from typing import Optional
 
 
-@dataclass
+@dataclass(slots=True)
 class RegisterUserRequest:
-    email: str = field(default_factory=str)
-    username: str = field(default_factory=str)
-    password: str = field(default_factory=str)
+    email: str
+    username: str
+    password: str
     
     first_name: Optional[str] = None
     last_name: Optional[str] = None
+
+
+@dataclass(slots=True)
+class RegisterUserResponse:
+    email: str
+    username: str
+    
+    first_name: str
+    last_name: str
