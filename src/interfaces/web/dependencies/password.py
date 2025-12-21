753a2e4cced75ac4f3ff0762192a8f4fe0d8d28d
Laurenz <ddanilenko350@gmail.com>
2025-12-21 16:26:20 +0200

    Add get_refresh_token_service, get_jwt_service + get_login_usecase

--- src/interfaces/web/dependencies/password.py
+++
@@ -1,5 +0,0 @@
-from src.application.services.password_service import PasswordService
-
-
-def get_password_service() -> PasswordService:
-    return PasswordService()
--- src/interfaces/web/dependencies/usecases.py
+++ src/interfaces/web/dependencies/usecases.py
@@ -1,9 +1,17 @@
 from fastapi import Depends
 
 from src.application.usecases.register_user import RegisterUserUseCase
+from src.application.usecases.login_user import LoginUserUseCase
 
-from .db import get_user_repository
-from .password import get_password_service
+from .db import (
+    get_user_repository,
+    get_refresh_token_repository
+)
+from .services import (
+    get_password_service,
+    get_refresh_token_service,
+    get_jwt_service,
+)
 
 
 async def get_register_user_usecase(
@@ -12,5 +20,21 @@ async def get_register_user_usecase(
 ):
     return RegisterUserUseCase(
         user_repository=user_repo,
+        password_service=password_service
+    )
+    
+    
+async def get_login_usecase(
+    user_repo = Depends(get_user_repository),
+    refresh_token_repo = Depends(get_refresh_token_repository),
+    refresh_token_service = Depends(get_refresh_token_service),
+    jwt_service = Depends(get_jwt_service),
+    password_service = Depends(get_password_service),
+):
+    return LoginUserUseCase(
+        user_repository=user_repo,
+        refresh_token_repository=refresh_token_repo,
+        refresh_token_service=refresh_token_service,
+        jwt_service=jwt_service,
         password_service=password_service
     )
