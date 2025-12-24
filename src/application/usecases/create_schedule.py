ae0d62a36f788d923f2e66fffa1f4a59e2f6abf5
Laurenz <ddanilenko350@gmail.com>
2025-12-24 13:22:06 +0200

    Add CreateScheduleUseCase + tests

--- src/application/usecases/create_schedule.py
+++ src/application/usecases/create_schedule.py
@@ -2,7 +2,9 @@ from src.application.dto.schedule import (
     ScheduleCreateRequest,
     ScheduleCreateResponse
 )
+from src.application.exceptions.auth import UserNotFoundError
 
+from src.domain.entities.schedule import Schedule
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.repositories.schedule_repository import ScheduleRepository
 
@@ -21,13 +23,24 @@ class CreateScheduleUseCase:
     async def execute(
         self,
         data: ScheduleCreateRequest,
-        user_id: int    
+        user_id: int
     ) -> ScheduleCreateResponse:
-        schedule = await self.schedule_repository.create(data)
-        
+        user = await self.user_repository.get_user_by_id(user_id)
+        if not user:
+            raise UserNotFoundError(user_id)
+
+        schedule = Schedule(
+            user_id=user_id,
+            name=data.name,
+            description=data.description,
+            type_schedule=data.type_schedule,
+        )
+
+        created = await self.schedule_repository.create(schedule)
+
         return ScheduleCreateResponse(
-            id=schedule.id,
-            name=schedule.name,
-            description=schedule.description or "",
-            type_schedule=schedule.type_schedule
+            id=created.id,
+            name=created.name,
+            description=created.description,
+            type_schedule=created.type_schedule,
         )
