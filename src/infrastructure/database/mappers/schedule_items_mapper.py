55e090b53d18a93389aa2c9eb4aacb8ff1cdc61d
Laurenz <ddanilenko350@gmail.com>
2025-12-26 19:07:14 +0200

    Add ScheduleItemsMapper

---
+++ src/infrastructure/database/mappers/schedule_items_mapper.py
@@ -0,0 +1,36 @@
+from src.domain.entities.schedule_items import ScheduleItems
+from src.infrastructure.database.models.schedule_items import ScheduleItemsModel
+
+class ScheduleItemsMapper:
+    @staticmethod
+    def to_orm(item: ScheduleItems, user_id: int) -> ScheduleItemsModel:
+        """
+        Converts the domain entity ScheduleItems into the ORM model ScheduleItemsModel.
+        """
+        return ScheduleItemsModel(
+            id=item.id,
+            schedule_id=item.schedule_id,
+            name=item.name,
+            description=item.description or "",
+            day_of_week=item.day_of_week,
+            time_start=item.time_start,
+            time_end=item.time_end,
+            user_id=user_id
+        )
+
+    @staticmethod
+    def to_domain(item_model: ScheduleItemsModel) -> ScheduleItems:
+        """
+        Converts the ORM model ScheduleItemsModel into the domain entity ScheduleItems.
+        """
+        return ScheduleItems(
+            id=item_model.id,
+            schedule_id=item_model.schedule_id,
+            name=item_model.name,
+            description=item_model.description if item_model.description else None,
+            day_of_week=item_model.day_of_week,
+            time_start=item_model.time_start,
+            time_end=item_model.time_end,
+            created_at=item_model.created_at,
+            updated_at=item_model.updated_at
+        )
